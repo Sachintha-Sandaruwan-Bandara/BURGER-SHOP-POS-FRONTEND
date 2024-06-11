d2dcf9e0c442bf1660486d370736e90aadf61bf5
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-06-11 20:36:55 +0530

    content update

--- js/db/db.js
+++ js/db/db.js
@@ -4,21 +4,21 @@ var customers = [
     name: "John Doe",
     address: "123 Main St",
     salary: "$50,000",
-    image: "/assets/john-doe.jpg",
+    image: "assets/john-doe.jpg",
   },
   {
     id: "C002",
     name: "Jane Smith",
     address: "456 Elm St",
     salary: "$60,000",
-    image: "/assets/rachel.jpg",
+    image: "assets/rachel.jpg",
   },
   {
     id: "C003",
     name: "Tom Jason",
     address: "45 D3R St",
     salary: "$60,000",
-    image: "/assets/jhon2.jpg",
+    image: "assets/jhon2.jpg",
   },
 ];
 
@@ -28,28 +28,28 @@ var items = [
     name: "Crispy Chiken Burger",
     qty: "30",
     price: "4.50$",
-    image: "/assets/burger.jpg",
+    image: "assets/burger.jpg",
   },
   {
     code: "I002",
     name: "Cheesy Beef Burger",
     qty: "23",
     price: "10$",
-    image: "/assets/burger2.jpg",
+    image: "assets/burger2.jpg",
   },
   {
     code: "I001",
     name: "Crispy Chiken Burger",
     qty: "30",
     price: "4.50$",
-    image: "/assets/burger.jpg",
+    image: "assets/burger.jpg",
   },
   {
     code: "I002",
     name: "Cheesy Beef Burger",
     qty: "23",
     price: "10$",
-    image: "/assets/burger2.jpg",
+    image: "assets/burger2.jpg",
   },
 ];
 
